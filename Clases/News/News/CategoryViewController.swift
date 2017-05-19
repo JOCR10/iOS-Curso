12fbf781c92cd1db0e98d679f0be5af23ce94b56
JOCR10 <localuser@MacBook-Pro-de-Luis.local>
2017-05-18 19:40:09 -0600

    Avance

--- Clases/News/News/CategoryViewController.swift
+++ Clases/News/News/CategoryViewController.swift
@@ -9,27 +9,43 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-
+    
+    @IBOutlet weak var categoryTableView: UITableView!
+    
+    var categories = [Category]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        categoryTableView.registerCustomCell(identifier: CategoryTableViewCell.getTableViewCellIdentifier())
+        initializeCategories()
+        self.title = "Categorías"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func initializeCategories() {
+        
+        let economyCategory = Category(name: "Economía", image: "economy")
+        let incidentCategory = Category(name: "Sucesos", image: "incident")
+        let sportsCategory = Category(name: "Deportes", image: "sports")
+        let technologyCategory = Category(name: "Tecnología", image: "technology")
+        
+        categories = [economyCategory, incidentCategory, sportsCategory, technologyCategory]
     }
-    */
+}
 
+extension CategoryViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = (tableView.dequeueReusableCell(withIdentifier: CategoryTableViewCell.getTableViewCellIdentifier())) as! CategoryTableViewCell
+        let category = categories[indexPath.row]
+        cell.setUpCell(category: category)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
 }
