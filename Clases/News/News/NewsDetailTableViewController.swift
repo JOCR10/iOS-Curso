e801496b7b533bfa86d3f7107ae8875f0aa54015
JOCR10 <localuser@MacBook-Pro-de-Luis.local>
2017-05-23 18:03:37 -0600

    Avance

---
+++ Clases/News/News/NewsDetailTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NewsDetailTableViewController.swift
+//  News
+//
+//  Created by Local User on 5/23/17.
+//  Copyright © 2017 Local User. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
